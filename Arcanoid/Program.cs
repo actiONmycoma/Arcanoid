bb96f3f5101dc1b89be1f9407fde097867128834
actiONmycoma <actionmycoma@gmail.com>
2023-04-10 16:20:11 +1000

    Add project files.

---
+++ Arcanoid/Program.cs
@@ -0,0 +1,34 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+
+namespace Arcanoid
+{
+    internal class Program
+    {
+        private static RenderWindow window;
+
+        static void Main(string[] args)
+        {
+            window = new RenderWindow(new VideoMode(800, 600), "Arcanoid");
+            window.SetFramerateLimit(60);
+            window.Closed += WindowClosed;
+
+
+            while (window.IsOpen == true)
+            {
+                window.Clear();
+
+                window.DispatchEvents();
+
+                window.Display();
+            }
+
+        }
+
+        private static void WindowClosed(object sender, EventArgs e)
+        {
+            window.Close();
+        }
+    }
+}
