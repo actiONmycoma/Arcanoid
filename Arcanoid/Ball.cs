e371b7204e48698614a217717e05394239693d62
actiONmycoma <actionmycoma@gmail.com>
2023-04-10 21:58:39 +1000

    Промежуточный результат с видео

---
+++ Arcanoid/Ball.cs
@@ -0,0 +1,59 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+using System;
+
+namespace Arcanoid
+{
+    internal class Ball
+    {
+        public Sprite sprite;
+
+        private float speed;
+        private Vector2f direction;
+
+        public Ball(Texture texture)
+        {
+            sprite = new Sprite(texture);
+        }
+
+        public void start(float speed, Vector2f direction)
+        {
+            if (this.speed != 0) return;
+
+            this.speed = speed;
+            this.direction = direction;
+        }
+
+        public void Move(Vector2i boundsPosition, Vector2i boundsSize)
+        {
+
+            sprite.Position += speed * direction;
+
+            if (sprite.Position.X < boundsPosition.X ||
+                sprite.Position.X > boundsSize.X - sprite.Texture.Size.X)
+            {
+                direction.X *= -1;
+            }
+
+            if (sprite.Position.Y < boundsPosition.Y)
+            {
+                direction.Y *= -1;
+            }
+
+        }
+
+        public bool CheckCollision(Sprite sprite)
+        {
+            if (this.sprite.GetGlobalBounds().Intersects(sprite.GetGlobalBounds()) == true)
+            {
+                direction.Y *= -1;
+                direction.X *= -1;
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- Arcanoid/Program.cs
+++ Arcanoid/Program.cs
@@ -16,6 +16,8 @@ namespace Arcanoid
         private static Sprite stick;
         private static Sprite[] blocks;
 
+        private static Ball ball;
+
         static void Main(string[] args)
         {
             window = new RenderWindow(new VideoMode(800, 600), "Arcanoid");
@@ -27,18 +29,36 @@ namespace Arcanoid
             stickTexture = new Texture("Stick.png");
 
             stick = new Sprite(stickTexture);
-            stick.Position = new Vector2f(400-stick.TextureRect.Width * 0.5f,550);
+            ball = new Ball(ballTexture);
+
+            stick.Position = new Vector2f(400 - stick.TextureRect.Width * 0.5f, 550);
+            ball.sprite.Position = new Vector2f(400, 400);
+
+
 
             while (window.IsOpen == true)
             {
                 window.Clear();
 
                 window.DispatchEvents();
+                                
+
+                if (Mouse.IsButtonPressed(Mouse.Button.Left) == true)
+                {
+                    ball.start(5, new Vector2f(0, -1));
+                }
+                                
+
+                ball.Move(new Vector2i(0,0),new Vector2i((int)window.Size.X, (int)window.Size.Y));
+                ball.CheckCollision(stick);
 
                 stick.Position = new Vector2f(Mouse.GetPosition(window).X - stick.TextureRect.Width * 0.5f,
                     stick.Position.Y);
 
+                
+
                 window.Draw(stick);
+                window.Draw(ball.sprite);
 
                 window.Display();
             }
