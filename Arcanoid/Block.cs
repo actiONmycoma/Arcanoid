8043045de31708d78487712cd2b40c8d66da2e8e
actiONmycoma <actionmycoma@gmail.com>
2023-04-11 18:05:33 +1000

    Add Block class

---
+++ Arcanoid/Block.cs
@@ -0,0 +1,15 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+using System;
+
+
+namespace Arcanoid
+{
+    internal class Block
+    {
+        public Sprite sprite;
+
+        private int breakCount;
+    }
+}
